e5ec1e5a9d6c7d4de69c066e3bc477044b9201eb
ronelzb <ronelzb@gmail.com>
2020-09-13 11:48:20 -0500

    reviewed binary_search

--- test_redo_past_algos/test.py
+++ test_redo_past_algos/test.py
@@ -1,10 +1,19 @@
-def factorial(x):
-    if x <= 0:
-        return 0
-    elif x == 1:
-        return 1
+def binary_search(arr, elem):
+    left = 0
+    right = len(arr) - 1
 
-    return x * factorial(x - 1)
+    while left <= right:
+        mid = (left + right) // 2
+        mid_value = arr[mid]
+
+        if mid_value == elem:
+            return mid
+        elif mid_value < elem:
+            left = mid + 1
+        else:  # mid_value > elem
+            right = mid - 1
+
+    return None
 
 
-print(factorial(4))
+print(binary_search([2, 3, 5, 6, 8, 9, 10, 12, 13, 15, 18], 3))
